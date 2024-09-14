43df212b3a6f538a75963c3512898a2a866cd6cb
Vojtěch Leischner <50262885+trackme518@users.noreply.github.com>
2024-09-14 13:52:59 +0200

    cleaned dataset

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Predict electricity prices based on weather and other data from sattelites using
 
 * Solar radiation
     * https://ads.atmosphere.copernicus.eu/cdsapp#!/dataset/cams-solar-radiation-timeseries?tab=form
-    * 
+    * Altitutde -999., universal time, 50.74424, 9.84271 - Germany, 49.94270, 15.11549 - Czechia
 * Electricity price data
     * https://www.spotmarketindex.cz/
+    * 1/1/2019-14/9/2024 daily averages
+* Merged data
+    * https://cds-beta.climate.copernicus.eu/datasets/sis-energy-derived-projections?tab=download
 
 
--- dataprep.py
+++ dataprep.py
@@ -12,48 +12,24 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, 
 
 import os
 file_path = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(file_path, 'dataset')
 file_path = os.path.join(file_path, '')
 print(file_path)
 
 #--------------------------------------------
-## Read weather data
-# https://www.copernicus.eu/en/copernicus-services
+## Read data
 pd.set_option('display.max.columns', 100)
 
 ## set the period of forcast in 2021
-start_mth = datetime.datetime(2021, 1, 1)
-end_mth = datetime.datetime(2021, 12, 31)
+start_mth = datetime.datetime(2023, 9, 14)
+end_mth = datetime.datetime(2024, 9, 14)
 
-## Read Weather Data
-weather = pd.read_csv(file_path+'weather_data.csv', encoding='utf-8')
-print(weather.head())
+## Read Data
+dataset = pd.read_csv(file_path+'merged_data_selection.csv', sep=',', encoding='utf-8')
+#dataset.columns = dataset.columns.str.lower()
+dataset['date'] = pd.to_datetime(dataset['date'], format='%d/%m/%Y')
+dataset['electricity_daily_average_CZ'] = dataset['electricity_daily_average_CZ'].astype(float)
+print(dataset.head())
 
-# Pivot the weather dataframe
-weather = weather.pivot(index='date', columns='station_name', values=['ev_transpiration','max_temp','min_temp','max_humid','min_humid','solar'])
-
-# Flatten multi-index columns
-weather.columns = ['_'.join(col).strip() for col in weather.columns.values]
-
-# Reset index
-weather = weather.reset_index()
-
-# Set date column as date type
-weather.date = pd.to_datetime(weather.date, format='%d/%m/%Y')
-
-#--------------------------------------------
-## Read Eletricity Demand Data - previosly filtered and formatted in open office
-# source https://www.spotmarketindex.cz/ 
-usage = pd.read_csv(file_path+'daily_avg_electricity_price_2024_filtered.csv', sep=',')
-usage.columns = usage.columns.str.lower()
-print(usage.head())
-
-## Filter only date and total demand columns
-selected_col = ['date','daily_average']
-usage = usage[selected_col]
-
-## convert column type
-#usage['date'] = pd.to_datetime(usage['date'])
-usage['date'] = pd.to_datetime(usage['date'], format='%d/%m/%Y')
-usage['daily_average'] = usage['daily_average'].astype(float)
 #--------------------------------------------
 
