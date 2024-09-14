565764ee0f0f7c4d48f9b42db1e39fcd37e8ee67
Vojtěch Leischner <50262885+trackme518@users.noreply.github.com>
2024-09-14 18:03:15 +0200

    readme update

--- README.md
+++ README.md
@@ -15,4 +15,45 @@ Predict electricity prices based on weather and other data from sattelites using
 * Merged data
     * https://cds-beta.climate.copernicus.eu/datasets/sis-energy-derived-projections?tab=download
 
+## Observations
+We can fairly accuretly predict electricity demand with the XGBoost Regressor - improved by scaler:
+Mean Squared Error = 37873863.09106111
+Mean Absolute Percentage Error =  0.16%
+R-squared = 0.98
+
+However it fails when trying to preditct the electricity prices taken from www.spotmarketindex.cz. Prices were taken outside the original dataset from spot market values. The Cassini dataset might be inaccurate or the spot prices rely on more variables then just the weather data - highly likely. When confiding the dataset to last year we get these results:
+
+XGBoost Regressor
+Mean Squared Error = 325364.434709683
+Mean Absolute Percentage Error =  54.00%
+R-squared = 0.45
+
+That is not accepteble. However when trying with DAM - day ahead market price (https://peaksubstation.com/day-ahead-vs-real-time-energy-markets/) index prices repoted by https://www.ote-cr.cz/en/statistics/yearly-market-report?date=2024-01-01 it seems better:
+
+Mean Squared Error = 4617.042284111203
+Mean Absolute Percentage Error =  24.72%
+R-squared = -0.04
+
+We have also tried "Random forest" algorithm (Mean Absolute Percentage Error =  25.99%) instead of "XGBRegressor" (Mean Absolute Percentage Error =  25.07%) and Linear regression (Mean Absolute Percentage Error =  28.76%).
+
+Still not good enough thought. Maybe we need to think more about the data.
+![variables correlation heatmap graph](dataset/correlationVariableMap_v2.png)
+
+From the variables correlation heatmap graph we can see some interesting relations. For example the electricity demand in Czech Republic has less impact then electricity demand in Deutschland and suprisingly in Slovakia as well. On the other hand we can see that wind speed, precipitation and on shore hydro energy power output is not much of a factor.
+
+By dropping these variables we improve the result slightly:
+
+Mean Squared Error = 433.7925148901215
+Mean Absolute Percentage Error =  24.59%
+R-squared = 0.46
+
+By restricting the data range to 1 year only we also get slightly better result:
+
+Mean Squared Error = 323.5808738722185
+Mean Absolute Percentage Error =  21.38%
+R-squared = 0.60
+
+
+
+
 
--- dataprep.py
+++ dataprep.py
@@ -10,6 +10,16 @@ from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
 
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+
 import os
 file_path = os.path.dirname(os.path.realpath(__file__))
 file_path = os.path.join(file_path, 'dataset')
@@ -20,16 +30,88 @@ print(file_path)
 ## Read data
 pd.set_option('display.max.columns', 100)
 
-## set the period of forcast in 2021
-start_mth = datetime.datetime(2023, 9, 14)
-end_mth = datetime.datetime(2024, 9, 14)
+## set the period of forcast
+start_mth = datetime.datetime(2023, 7, 31)
+end_mth = datetime.datetime(2024, 7, 31)
 
 ## Read Data
-dataset = pd.read_csv(file_path+'merged_data_selection.csv', sep=',', encoding='utf-8')
+dataset = pd.read_csv(file_path+'merged_data_selection3.csv', sep=',', encoding='utf-8')
 #dataset.columns = dataset.columns.str.lower()
 dataset['date'] = pd.to_datetime(dataset['date'], format='%d/%m/%Y')
-dataset['electricity_daily_average_CZ'] = dataset['electricity_daily_average_CZ'].astype(float)
+#dataset['electricity_daily_average_CZ'] = dataset['electricity_daily_average_CZ'].astype(float)
+
+## Filter dataset within start and end month
+dataset = dataset[( dataset['date']>=start_mth) & (dataset['date'] <= end_mth)]
+
 print(dataset.head())
 
 #--------------------------------------------
+#"""
+## correlation heat map
+merge_wo_date = dataset.drop(columns='date')
+corr_matrix = merge_wo_date.corr()
 
+##plot the heat map
+plt.figure(figsize=(15,8))
+sns.heatmap(corr_matrix, annot = True, cmap = 'coolwarm', fmt = ".2f", linewidths=.5, annot_kws={'size':7})
+plt.xticks(fontsize=8)
+plt.yticks(fontsize=8)
+plt.title('Correlation Heatmap')
+plt.show()
+
+#"""
+#--------------------------------------------
+## Split data into feature and target variable
+
+#dataset.drop(columns=['precipitation_DE', 'precipitation_HU', 'precipitation_RO', 'precipitation_SK', 'electricity_daily_average_CZ', 'wind_speed_CZ','wind_speed_DE','wind_speed_HU','wind_speed_RO','wind_speed_SK'])
+#dataset = dataset.drop(columns=['electricity_daily_average_CZ'])
+X = dataset.drop(columns=['date','price_mwh_eur_DAM']) 
+y = dataset['price_mwh_eur_DAM']
+
+#X = dataset.drop(columns=['date','electricity_demand_CZ'])
+#y = dataset['electricity_demand_CZ']
+
+## Split data into train and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+## Set the model dictionary
+"""
+models = {
+'Linear Regression': LinearRegression()
+, 'Random Forest Regressor': RandomForestRegressor(random_state = 42)
+, 'XGBoost Regressor' : XGBRegressor(random_state = 42)
+}
+"""
+
+model = XGBRegressor(random_state = 42)
+#model = RandomForestRegressor(random_state = 42)
+#model = LinearRegression()
+
+# Standardize features by removing the mean and scaling to unit variance
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+## Predict and Evaluation
+model.fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
+mse = mean_squared_error(y_test, y_pred)
+mape = mean_absolute_percentage_error(y_test, y_pred) * 100
+r_squared = r2_score(y_test, y_pred)
+
+## print model accuracy
+print(f"Mean Squared Error = {mse}")
+print(f"Mean Absolute Percentage Error = {mape: .2f}%")
+print(f"R-squared = {r_squared:.2f}")
+
+## plot scatter, and line comparing test data and prediction
+plt.figure(figsize=(6,6))
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+p1 = max(max(y_pred), max(y_test))
+p2 = min(min(y_pred), min(y_test))
+plt.plot([p1, p2], [p1, p2], color = 'r', linestyle = '-', lw=2)
+plt.grid(True)
+plt.show()
